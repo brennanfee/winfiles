a2335fc8e2e294bda5702c090edf1bc36288f218
Brennan Fee <github@todaytechsoft.biz>
2018-05-19 23:16:27 -0500

    Remove wsltty batch files, update wsltty settings
    Solidifying on wsltty as Windows terminal for WSL shells.  May switch to
    hyper if they get the mouse working for tmux.

--- bin/Update-Vim-Plugins.ps1
+++ bin/Update-Vim-Plugins.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env powershell.exe
 
-&"C:\Program Files (x86)\vim\vim80\gvim.exe" -u "$env:userprofile\.vimrc.bundles" +PlugInstall +PlugClean! +qa -
+&"C:\Program Files (x86)\vim\vim80\gvim.exe" -N -u "$env:userprofile\.vimrc.bundles" +PlugInstall +PlugClean! +qa -
--- bin/WSL.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Start mintty terminal for WSL package  in current directory
-%LOCALAPPDATA%\wsltty\bin\mintty.exe -i "%LOCALAPPDATA%\wsltty\wsl.ico" --WSL= --configdir="%APPDATA%\wsltty" tmux
