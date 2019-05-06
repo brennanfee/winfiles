19ecc92f32a50a23b3172eec6ad7ce6da9b11b83
Brennan Fee <febrenna@amazon.com>
2019-05-06 13:38:28 -0500

    Adding FileAndRegistryUtilities module

---
+++ powershell-modules/FileAndRegistryUtilities/FileAndRegistryUtilities.psd1
@@ -0,0 +1,15 @@
+@{
+    ModuleVersion = '1.0'
+
+    GUID = 'ce8a1507-9915-41e7-98e5-b82f36c63438'
+
+    Author = 'Brennan Fee'
+
+    Copyright = '(c) 2019 Brennan Fee. All rights reserved.'
+
+    Description = 'Utility methods to manipulate files and the registry.'
+
+    FileList = @('FileAndRegistryUtilities.psm1')
+
+    FunctionsToExport = @('Set-*', 'New-*')
+}
---
+++ powershell-modules/FileAndRegistryUtilities/FileAndRegistryUtilities.psm1
@@ -0,0 +1,194 @@
+#!/usr/bin/env powershell.exe
+#Requires -Version 5
+
+# TODO: Add documentation to each function
+
+## Registry
+function Set-RegistryBool {
+    [CmdletBinding()]
+    param(
+        [string]$Key,
+        [string]$ValueName,
+        [bool]$Value
+    )
+
+    if ($Value) {
+        Set-RegistryValue $Key $ValueName "0" "DWord"
+    }
+    else {
+        Set-RegistryValue $Key $ValueName "1" "DWord"
+    }
+}
+
+function Set-RegistryInt {
+    [CmdletBinding()]
+    param(
+        [string]$Key,
+        [string]$ValueName,
+        [int]$Value
+    )
+
+    Set-RegistryValue $Key $ValueName $Value "DWord"
+}
+
+function Set-RegistryString {
+    [CmdletBinding()]
+    param(
+        [string]$Key,
+        [string]$ValueName,
+        [string]$Value
+    )
+
+    Set-RegistryValue $Key $ValueName $Value "String"
+}
+
+function Set-RegistryStringExpand {
+    [CmdletBinding()]
+    param(
+        [string]$Key,
+        [string]$ValueName,
+        [string]$Value
+    )
+
+    Set-RegistryValue $Key $ValueName $Value "ExpandString"
+}
+
+function Set-RegistryStringMulti {
+    [CmdletBinding()]
+    param(
+        [string]$Key,
+        [string]$ValueName,
+        [string[]]$Values
+    )
+
+    Set-RegistryValue $Key $ValueName $Values "MultiString"
+}
+
+function Set-RegistryValue {
+    [CmdletBinding()]
+    param(
+        [string]$Key,
+        [string]$ValueName,
+        $Value,
+        [string]$DataType
+    )
+
+    if (!(Test-Path $Key)) {
+        New-Item -ItemType Directory -Force -Path $Key
+    }
+    New-ItemProperty -Path $Key -Name $ValueName -Value $Value -PropertyType $DataType -ErrorAction SilentlyContinue
+    Set-ItemProperty -Path $Key -Name $ValueName -Value $Value
+}
+
+## Taking ownership
+
+function Set-RegistryOwnership {
+    [CmdletBinding()]
+    param(
+        [string]$Key
+    )
+
+    # TODO does not work for all root keys yet
+    switch ($Key.split('\')[0]) {
+        "HKEY_CLASSES_ROOT" {
+            $reg = [Microsoft.Win32.Registry]::ClassesRoot
+            $Key = $Key.substring(18)
+        }
+        "HKEY_CURRENT_USER" {
+            $reg = [Microsoft.Win32.Registry]::CurrentUser
+            $Key = $Key.substring(18)
+        }
+        "HKEY_LOCAL_MACHINE" {
+            $reg = [Microsoft.Win32.Registry]::LocalMachine
+            $Key = $Key.substring(19)
+        }
+    }
+
+    # get administraor group
+    $admins = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")
+    $admins = $admins.Translate([System.Security.Principal.NTAccount])
+
+    # set owner
+    $Key = $reg.OpenSubKey($Key, "ReadWriteSubTree", "TakeOwnership")
+    $acl = $Key.GetAccessControl()
+    $acl.SetOwner($admins)
+    $Key.SetAccessControl($acl)
+
+    # set FullControl
+    $acl = $key.GetAccessControl()
+    $rule = New-Object System.Security.AccessControl.RegistryAccessRule($admins, "FullControl", "Allow")
+    $acl.SetAccessRule($rule)
+    $key.SetAccessControl($acl)
+}
+
+function Set-FileOwnership {
+    [CmdletBinding()]
+    param(
+        [string]$File
+    )
+
+    takeown.exe /A /F $File
+    $acl = Get-Acl $File
+
+    # get administraor group
+    $admins = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")
+    $admins = $admins.Translate([System.Security.Principal.NTAccount])
+
+    # add NT Authority\SYSTEM
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($admins, "FullControl", "None", "None", "Allow")
+    $acl.AddAccessRule($rule)
+
+    Set-Acl -Path $File -AclObject $acl
+}
+
+function Set-FolderOwnership {
+    [CmdletBinding()]
+    param(
+        [string]$Path
+    )
+
+    Set-FileOwnership $Path
+    foreach ($item in Get-ChildItem $Path) {
+        if (Test-Path $item -PathType "Container") {
+            Set-FolderOwnership $item.FullName
+        }
+        else {
+            Set-FileOwnership $item.FullName
+        }
+    }
+}
+Set-Alias Set-DirectoryOwnership Set-FolderOwnership
+
+# Symbolic links
+
+function New-SymbolicLink-Safe {
+    [CmdletBinding()]
+    param(
+        [string]$Link,
+        [string]$Target
+    )
+
+    # This version will not do anything if the a link or file already exists
+    # at the link point
+    If (!(Test-Path("$Link"))) {
+#        cmd /c mklink "$Link" "$Target"
+        New-Item -ItemType SymbolicLink -Name "$Link" -Target "$Target"
+    }
+}
+
+function New-SymbolicLink {
+    [CmdletBinding()]
+    param(
+        [string]$Link,
+        [string]$Target
+    )
+
+    # This version will first remove the link or file if it exists and will
+    # then re-create the link
+    If (Test-Path("$Link")) {
+        Remove-Item -Path "$Link"
+    }
+
+#    cmd /c mklink "$Link" "$Target"
+    New-Item -ItemType SymbolicLink -Name "$Link" -Target "$Target"
+}
