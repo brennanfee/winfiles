307c377b9f74653a64423f012806c7e4dbb498b5
Brennan Fee <brennan@github.mailbots.org>
2019-05-11 00:35:20 -0500

    Adding version sorting to all ls operations

--- powershell-modules/MyCustomProfileUtilities/Aliases.ps1
+++ powershell-modules/MyCustomProfileUtilities/Aliases.ps1
@@ -5,30 +5,27 @@ Set-StrictMode -Version 2.0
 Set-Alias e Edit-File
 Set-Alias edit Edit-File
 
-function Get-ListingWslLs { wsl.exe ls --color=auto --group-directories-first }
-#TODO: Neither of these are working
-Set-Alias ls Get-ListingWslLs -Option AllScope
-Set-Alias sl Get-ListingWslLs
+function Get-ListingWslLs { wsl.exe ls -v --color=auto --group-directories-first }
 
-function Get-ListingWslLss { wsl.exe ls -1 --color=auto --group-directories-first }
+function Get-ListingWslLss { wsl.exe ls -1v --color=auto --group-directories-first }
 Set-Alias lss Get-ListingWslLss
 
-function Get-ListingWslLa { wsl.exe ls -A --color=auto --group-directories-first }
+function Get-ListingWslLa { wsl.exe ls -Av --color=auto --group-directories-first }
 Set-Alias la Get-ListingWslLa
 
-function Get-ListingWslLl { wsl.exe ls -oh --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslLl { wsl.exe ls -ohv --color=auto --group-directories-first --time-style=long-iso }
 Set-Alias ll Get-ListingWslLl
 
-function Get-ListingWslLla { wsl.exe ls -ohA --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslLla { wsl.exe ls -ohAv --color=auto --group-directories-first --time-style=long-iso }
 Set-Alias lla Get-ListingWslLla
 
-function Get-ListingWslLls { wsl.exe ls -lhA --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslLls { wsl.exe ls -lhAv --color=auto --group-directories-first --time-style=long-iso }
 Set-Alias lls Get-ListingWslLls
 
-function Get-ListingWslLdir { wsl.exe ls -ohA --color=never --group-directories-first --time-style=long-iso | wsl.exe grep --color=never "^d" }
+function Get-ListingWslLdir { wsl.exe ls -ohAv --color=never --group-directories-first --time-style=long-iso | wsl.exe grep --color=never "^d" }
 Set-Alias ldir Get-ListingWslLdir
 
-function Get-ListingWslVdir { wsl.exe ls -lhA --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslVdir { wsl.exe ls -lhAv --color=auto --group-directories-first --time-style=long-iso }
 Set-Alias vdir Get-ListingWslVdir
 
 function Get-ListingWslTree { wsl.exe tree -C }
