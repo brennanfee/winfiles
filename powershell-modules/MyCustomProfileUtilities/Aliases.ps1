2f8243c2cda39ce2be5635b2b2445777a9d80fb2
Brennan Fee <brennan@github.mailbots.org>
2019-05-16 22:16:21 -0500

    Fixing ls aliases to support arguments
    The way I had it before you couldn't filter.  Now supports passing
    arguments in with those commands.

--- powershell-modules/MyCustomProfileUtilities/Aliases.ps1
+++ powershell-modules/MyCustomProfileUtilities/Aliases.ps1
@@ -5,27 +5,27 @@ Set-StrictMode -Version 2.0
 Set-Alias e Edit-File
 Set-Alias edit Edit-File
 
-function Get-ListingWslLs { wsl.exe ls -v --color=auto --group-directories-first }
+function Get-ListingWslLs { wsl.exe ls -v --color=auto --group-directories-first $args }
 
-function Get-ListingWslLss { wsl.exe ls -1v --color=auto --group-directories-first }
+function Get-ListingWslLss { wsl.exe ls -1v --color=auto --group-directories-first $args }
 Set-Alias lss Get-ListingWslLss
 
-function Get-ListingWslLa { wsl.exe ls -Av --color=auto --group-directories-first }
+function Get-ListingWslLa { wsl.exe ls -Av --color=auto --group-directories-first $args }
 Set-Alias la Get-ListingWslLa
 
-function Get-ListingWslLl { wsl.exe ls -ohv --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslLl { wsl.exe ls -ohv --color=auto --group-directories-first --time-style=long-iso $args }
 Set-Alias ll Get-ListingWslLl
 
-function Get-ListingWslLla { wsl.exe ls -ohAv --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslLla { wsl.exe ls -ohAv --color=auto --group-directories-first --time-style=long-iso $args }
 Set-Alias lla Get-ListingWslLla
 
-function Get-ListingWslLls { wsl.exe ls -lhAv --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslLls { wsl.exe ls -lhAv --color=auto --group-directories-first --time-style=long-iso $args }
 Set-Alias lls Get-ListingWslLls
 
-function Get-ListingWslLdir { wsl.exe ls -ohAv --color=never --group-directories-first --time-style=long-iso | wsl.exe grep --color=never "^d" }
+function Get-ListingWslLdir { wsl.exe ls -ohAv --color=never --group-directories-first --time-style=long-iso $args | wsl.exe grep --color=never "^d" }
 Set-Alias ldir Get-ListingWslLdir
 
-function Get-ListingWslVdir { wsl.exe ls -lhAv --color=auto --group-directories-first --time-style=long-iso }
+function Get-ListingWslVdir { wsl.exe ls -lhAv --color=auto --group-directories-first --time-style=long-iso  $args }
 Set-Alias vdir Get-ListingWslVdir
 
 function Get-ListingWslTree { wsl.exe tree -C }
