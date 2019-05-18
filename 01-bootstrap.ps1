8f8fd72dab4dac731e99d8e59dc0307cac0f4e83
Brennan Fee <brennan@github.mailbots.org>
2019-05-18 17:25:46 -0500

    Adding 01-bootstrap

---
+++ 01-bootstrap.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env powershell.exe
+#Requires -Version 5
+#Requires -RunAsAdministrator
+Set-StrictMode -Version 2.0
+
+# Note, these may need to be run BEFORE this script
+Set-ExecutionPolicy Unrestricted -scope LocalMachine -Force -ErrorAction Ignore
+Set-ExecutionPolicy Unrestricted -scope CurrentUser -Force -ErrorAction Ignore
+
