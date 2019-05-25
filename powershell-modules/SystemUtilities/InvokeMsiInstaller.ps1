df4117bce6e27d1dd734a01ee9e437194c49c499
Brennan Fee <brennan@github.mailbots.org>
2019-05-25 15:42:59 -0500

    Invoke msi methods now allow passing log file
    The method does provide a default, but it can now be passed in.

--- powershell-modules/SystemUtilities/InvokeMsiInstaller.ps1
+++ powershell-modules/SystemUtilities/InvokeMsiInstaller.ps1
@@ -5,46 +5,45 @@ Set-StrictMode -Version 2.0
 function Invoke-MsiInstaller {
     [CmdletBinding()]
     param(
+        [ValidateScript( { Test-Path $_ -PathType "Leaf" })]
         [string]$Msi,
-        [string]$AppName = ""
+        [string]$LogFile = ""
     )
 
-    if ([string]::IsNullOrEmpty($AppName)) {
-        $AppName = [System.IO.Path]::GetFileNameWithoutExtension($Msi) +
-            [System.IO.Path]::GetExtension($Msi)
+    $installer = [System.IO.Path]::GetFileNameWithoutExtension($Msi) +
+        [System.IO.Path]::GetExtension($Msi)
+
+    Write-Host "Installing $installer"
+    if ([string]::IsNullOrEmpty($LogFile)) {
+        $LogFile = Get-LogFile "msi-$installer.log"
     }
 
-    Write-Host "Install $AppName"
-    $outputPath = "$PSScriptRoot\..\..\output"
-    $logFile = "$outputPath\install-$AppName.log"
-
-    $MsiArguments = @(
+    $arguments = @(
         "/i"
         ('"{0}"' -f $Msi)
         "/qn"
         "/norestart"
-        "/L*v"
-        $logFile
+        "/L*v+"
+        ('"{0}"' -f $LogFile)
     )
-    Start-Process "msiexec.exe" -ArgumentList $MsiArguments -Wait -NoNewWindow
+
+    Start-Process -Wait -NoNewWindow -FilePath "msiexec.exe" -ArgumentList $arguments
 }
 
 function Invoke-MsiInstallerFromUrl {
     [CmdletBinding()]
     param(
         [string]$Url,
-        [string]$AppName = ""
+        [string]$LogFile = ""
     )
 
-    if ([string]::IsNullOrEmpty($AppName)) {
-        $AppName = [System.IO.Path]::GetFileNameWithoutExtension($Url) +
-            [System.IO.Path]::GetExtension($Url)
-    }
+    $installer = [System.IO.Path]::GetFileNameWithoutExtension($Msi) +
+        [System.IO.Path]::GetExtension($Msi)
 
     $tempFileName = [System.IO.Path]::GetTempFileName();
 
-    Write-Host "Downloading $AppName"
+    Write-Host "Downloading $installer"
     Invoke-WebRequest $Url -UseBasicParsing -o $tempFileName
 
-    Invoke-MsiInstaller $tempFileName $AppName
+    Invoke-MsiInstaller $tempFileName $LogFile
 }
