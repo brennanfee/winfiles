dd790886b62679c5681dd80826302c07f64f8bb4
Brennan Fee <brennan@github.mailbots.org>
2019-05-25 22:55:01 -0500

    Adding some windows update settings

--- bootstrapScripts/05-windowsUpdates.ps1
+++ bootstrapScripts/05-windowsUpdates.ps1
@@ -17,3 +17,11 @@ else {
     Set-RegistryInt "$updateKey" "ActiveHoursStart" 8
     Set-RegistryInt "$updateKey" "ActiveHoursEnd" 22
 }
+
+# Turn on getting updates for Microsoft products
+$key = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services"
+Set-RegistryInt "$key\7971F918-A847-4430-9279-4A52D1EFE18D" "RegisteredWithAU" 1
+
+# Turn on notifications for need to restart to finish updates
+$key = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings"
+Set-RegistryInt "$key" "RestartNotificationsAllowed2" 1
