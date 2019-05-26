8c8dc73f699b9aea05aa2e3e484522ec5a2bc05d
Brennan Fee <brennan@github.mailbots.org>
2019-05-25 21:16:51 -0500

    Setting up a baseline script, removing AppGet
    The AppGet install happens as part of the profile setup now.  The
    baseline verifies everything.

---
+++ bootstrapScripts/01-verifyBaseline.ps1
@@ -0,0 +1,77 @@
+#!/usr/bin/env pwsh.exe
+#Requires -Version 5
+#Requires -RunAsAdministrator
+Set-StrictMode -Version 2.0
+
+# This script verifies the baseline and corrects what is needed.
+# It is ALWAYS expected that users run 01-setup-profile.ps1 but not
+# necessary 00-pull.ps1.  This will cover the situation where they did
+# skip 00-pull.ps1.  As a result, there is some code duplication between
+# this script and 00-pull.ps1.  Changes to 00-pull.ps1 may need to be copied
+# to here.
+
+Write-Host "Verifying baseline"
+
+# Setup the profile environment variable
+Write-Host "Setting profile location"
+Set-MyCustomProfileLocation -Force
+
+# Check if scoop is already installed
+if (-not (Test-Path "$env:ProfilePath\scoop\shims\scoop")) {
+    Write-Host "Scoop missing, preparing for install"
+    [environment]::SetEnvironmentVariable('SCOOP', "$env:ProfilePath\scoop", 'User')
+    $env:SCOOP = "$env:ProfilePath\scoop"
+    [environment]::SetEnvironmentVariable('SCOOP_GLOBAL', 'C:\scoop-global', 'Machine')
+    $env:SCOOP_GLOBAL = 'C:\scoop-global'
+
+    # Install scoop
+    Write-Host ""
+    Set-StrictMode -Off # Need to turn StrictMode off because Scoop script has errors
+    Invoke-Expression ((Invoke-WebRequest -UseBasicParsing -Uri 'https://get.scoop.sh').Content)
+    Set-StrictMode -Version 2.0
+    Write-Host ""
+
+    # Scoop configuration
+    Add-MpPreference -ExclusionPath $env:SCOOP
+    Add-MpPreference -ExclusionPath $env:SCOOP_GLOBAL
+    Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+
+    Write-Host "Scoop installed." -ForegroundColor "Green"
+}
+else {
+    Write-Host "Scoop already installed." -ForegroundColor "Green"
+}
+
+# Check if git is already installed
+if (-not (Test-Path "$env:SCOOP_GLOBAL\shims\git.exe")) {
+    Write-Host "Git missing, preparing for install using scoop."
+
+    Write-Host ""
+    Invoke-Expression "scoop install --global sudo 7zip git innounp dark which"
+    Write-Host ""
+
+    [environment]::SetEnvironmentVariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+    $env:GIT_SSH = (resolve-path (scoop which ssh))
+    Write-Host "Git installed." -ForegroundColor "Green"
+}
+else {
+    Write-Host "Git already installed." -ForegroundColor "Green"
+}
+
+# I know it might look odd to check for pulling the Winfiles repo.  But,
+# while the user is obviously running these files from within a copy of the Winfiles,
+# there is no gaurantee they are in the right location.
+
+### Pull the WinFiles repo from GitHub
+if (-not (Test-Path "$env:ProfilePath\winfiles\README.md")) {
+    Write-Host "Winfiles missing, preparing to clone"
+
+    Write-Host ""
+    Invoke-Expression "git clone --recurse-submodules https://github.com/brennanfee/winfiles.git `"$env:ProfilePath\winfiles`""
+    Write-Host ""
+
+    Write-Host "Finished cloning winfiles." -ForegroundColor "Green"
+}
+else {
+    Write-Host "Winfiles already set up." -ForegroundColor "Green"
+}
--- bootstrapScripts/02-setupForInstalls.ps1
+++ bootstrapScripts/02-setupForInstalls.ps1
@@ -3,16 +3,6 @@
 #Requires -RunAsAdministrator
 Set-StrictMode -Version 2.0
 
-# Copy paste.exe to C:\Windows\system32 directory
-Write-Host "Checking for paste.exe"
-if (-not (Test-Path "$env:SystemRoot\system32\paste.exe")) {
-    Write-Host "Copying paste.exe"
-    Copy-Item -Path "$env:ProfilePath\winfiles\installs\paste.exe" -Destination "$env:SystemRoot\system32"
-}
-else {
-    Write-Host "Paste.exe already in place"
-}
-
 # Setup "extras" repo for scoop
 Write-Host "Setting up scoop buckets"
 Add-ScoopBucket "main"
@@ -20,15 +10,3 @@ Add-ScoopBucket "extras"
 Add-ScoopBucket "nerd-fonts"
 Add-ScoopBucket "java"
 Write-Host "Scoop buckets configured"
-
-# Install AppGet
-Write-Host "Checking for AppGet"
-if (-not (Test-Path "C:\ProgramData\AppGet\bin\appget.exe")) {
-    Write-Host "Installing AppGet"
-    $logFile="$env:ProfilePath\logs\winfiles\appget-install.log"
-    $command = "$env:ProfilePath\winfiles\installs\appget.exe /VERYSILENT /SUPPRESSMSGBOXES /SP `"/LOG=$logFile`""
-    Invoke-Expression -command $command
-}
-else {
-    Write-Host "AppGet already installed"
-}
