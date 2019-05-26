f743548cf79c992286697de30f11f283a880c084
Brennan Fee <brennan@github.mailbots.org>
2019-05-25 20:57:19 -0500

    Removing set of execution policies from bootstrap

--- 02-bootstrap.ps1
+++ 02-bootstrap.ps1
@@ -12,10 +12,6 @@ Param(
 Set-StrictMode -Version 2.0
 $global:InstallType = $InstallType
 
-# Note, these may need to be run BEFORE this script
-Set-ExecutionPolicy Unrestricted -scope LocalMachine -Force -ErrorAction Ignore
-Set-ExecutionPolicy Unrestricted -scope CurrentUser -Force -ErrorAction Ignore
-
 $logPath = "$env:ProfilePath\logs\winfiles"
 $logFile = "$logPath\bootstrap.log"
 Write-Log $logFile "----------"
@@ -24,7 +20,7 @@ Write-LogAndConsole $logFile "Install type: $global:InstallType"
 
 Get-ChildItem "$PSScriptRoot\bootstrapScripts" -File -Filter "*.ps1" | Sort-Object "FullName" | ForEach-Object {
     $script = $_.FullName
-    $scriptName = [io.path]::GetFileNameWithoutExtension("$script")
+    $scriptName = [System.IO.Path]::GetFileNameWithoutExtension("$script")
 
     Write-Host ""
     Write-LogAndConsole $logFile "Running script: $script"
@@ -42,6 +38,6 @@ Get-ChildItem "$PSScriptRoot\bootstrapScripts" -File -Filter "*.ps1" | Sort-Obje
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
 
 Write-Host ""
-Write-LogAndConsole $logFile "A reboot may be necessary for settings to take effect." -Color "Yellow"
+Write-LogAndConsole $logFile "A reboot will be necessary for settings to take effect." -Color "Yellow"
 Write-Host ""
 Write-LogAndConsole $logFile "Complete" -Color "Green"
