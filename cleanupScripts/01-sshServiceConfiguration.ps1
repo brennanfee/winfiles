7409cc205214a932dfb2b83d168fc7c8cd3b109f
Brennan Fee <febrenna@amazon.com>
2019-05-28 13:42:26 -0500

    Adding some logging to cleanup script

--- cleanupScripts/01-sshServiceConfiguration.ps1
+++ cleanupScripts/01-sshServiceConfiguration.ps1
@@ -3,11 +3,13 @@
 #Requires -RunAsAdministrator
 Set-StrictMode -Version 2.0
 
+Write-Host "Checking sshd service"
 if (Get-Service -Name sshd -ErrorAction SilentlyContinue) {
     Start-Service -Name sshd
     Set-Service -Name sshd -StartupType 'Automatic' | Out-Null
 }
 
+Write-Host "Checking ssh-agent service"
 if (Get-Service -Name ssh-agent -ErrorAction SilentlyContinue) {
     Start-Service -Name ssh-agent
     Set-Service -Name ssh-agent -StartupType 'Automatic'  | Out-Null
