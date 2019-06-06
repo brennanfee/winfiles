10a3569091bd6e1763c97aaf4921fafc534de76d
Brennan Fee <brennan@github.mailbots.org>
2019-06-06 00:14:47 -0500

    Removing hyper.js settings
    It's not likely I'm going to use hyper.js any more.  Besides, if I do
    again in the future it has been updated significantly enough that I
    should start the settings over again.

--- scripts/Symlinks/Hyper-Settings.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh.exe
-Set-StrictMode -Version Latest
-
-Import-Module -DisableNameChecking "$PSScriptRoot\..\Utilities\FileAndRegistryUtilities.psm1"
-
-Make-Link "$env:userprofile\.hyper.js" "$env:userprofile\winFiles\settings\hyper.js"
-Make-Link "$env:userprofile\.hyper.css" "$env:userprofile\winFiles\dotfiles\rcs\hyper.css"
