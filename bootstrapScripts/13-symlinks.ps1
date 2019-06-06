5416709289ab59301badeb8089b845f3f5d498f5
Brennan Fee <brennan@github.mailbots.org>
2019-06-06 00:09:43 -0500

    Adding symlink of Git Config

---
+++ bootstrapScripts/13-symlinks.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh.exe
+#Requires -Version 5
+#Requires -RunAsAdministrator
+Set-StrictMode -Version 2.0
+
+$winFiles = "$env:ProfilePath\winfiles"
+
+########  Git Configuration
+Write-Host "Linking Git Configuration"
+
+Remove-Item -Path "$env:USERPROFILE\.gitconfig"
+New-SymbolicLink "$env:USERPROFILE\.config\git\config" "$winFiles\dotfiles\rcs\gitconfig"
+New-SymbolicLink "$env:USERPROFILE\.config\git\gitconfig.os" "$winFiles\settings\gitconfig.os"
+
+$tag = $global:InstallType
+if ($global:InstallType == "gaming") {
+    $tag = "home"
+}
+
+$target = "$winFiles\dotfiles\rcs\tag-$tag\config\git\gitconfig.user"
+New-SymbolicLink "$env:USERPROFILE\.config\git\gitconfig.user" $target
