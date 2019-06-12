b23c059fd235317fd5eb17d36c8c8ad33a029fd0
Brennan Fee <brennan@github.mailbots.org>
2019-06-11 19:37:45 -0500

    Fixed ability for ansible to use apt
    Must set which python interpreter you want to use.  In this case I am
    pointing it at the system interpreter so it can pick up the system apt
    package.

--- wsl/ubuntu/03-setup-ansible.sh
+++ wsl/ubuntu/03-setup-ansible.sh
@@ -8,11 +8,12 @@ IFS=$'\n\t'
 # and the Ubuntu shell (no version) shells available in the Windows Store.
 
 # After running this script the following steps should be taken:
-# 1.  Tweak the ~/.rcrc if needed, run `mkrc -o "$HOME/.rcrc"` if desired
-# 2.  Run `rcup`
-# 3.  Close and reload the shell
-# 4.  Run the ansible script in this same folder.
-#    * Command:  (tbd ansible cmd here)
-# 5.  Run `asdf install` - this will take very LONG time
-# 6.  Close and reload the shell
-# 7.  That's it, everything should now be fully configured
+# 1.  Run `asdf install` - this will take very LONG time
+# 2.  Close and reload the shell
+
+ansible-playbook -b -K main.yml -e ansible_python_interpreter=/usr/bin/python3
+
+echo ""
+echo "You can now run `asdf install`, that will take a LONG time."
+echo "Once complete, close and reload the shell and setup is complete."
+echo ""
