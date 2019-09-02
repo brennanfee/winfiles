b18545cfa86f8a062fb99c64e547dceb7b0ce914
Brennan Fee <brennan@github.mailbots.org>
2019-09-01 19:10:32 -0500

    Adjusting AppGet installed apps
    Added in a few that I am moving over from scoop.  Removed a few I am not
    using.

--- bootstrapScripts/11-appGetInstalls.ps1
+++ bootstrapScripts/11-appGetInstalls.ps1
@@ -16,6 +16,7 @@ $apps = @(
     "amazon-workspaces"
     "autohotkey"
     "avidemux"
+    "aws-cli"
     #    "aws-tools-for-dotnet" # Needs to be manual, after install of Visual Studio
     "beyond-compare"
     "bitwarden"
@@ -24,29 +25,36 @@ $apps = @(
     "cmake"
     "devdocs"
     "dotnet-core-sdk"
-    "dotnet"
+    "dotnet-sdk"
     #    "dropbox"
     "etcher"
+    "evernote"
     "firefox"
+    "ghostscript"
+    "go"
     "hack-fonts"
     "handbrake"
+    "hashcheck"
+    "imagemagick"
     "jetbrains-toolbox"
     "kindle"
     "megasync"
-    #"miktex" -> scoop
-    #"pandoc" -> scoop
+    "miktex"
+    "node-lts"
+    "pandoc"
     "pencil"
+    "perl"
     "pia"
     "plex-media-player"
     "postman"
-    #"pushbullet" # use Wavebox intead
+    "python"
     "rainmeter"
     "scribus"
     "signal"
-    #"skype"
     #"spotify" # use the Windows Store version instead?
     "sqlectron"
-    #"texmaker" -> scoop
+    "teams"
+    "texmaker"
     "visual-studio-code"
     "vlc"
     "wavebox"
@@ -60,7 +68,7 @@ foreach ($app in $apps) {
 if (-not ($installType -eq "work")) {
     Write-Host "Installing applications using AppGet - Non-Work"
     $apps = @(
-        #        qbittorrent
+        "qbittorrent"
         #        transmission
         #        transmission-remote-gui
         "myharmony"
@@ -75,7 +83,6 @@ if ($installType -eq "gaming") {
     Write-Host "Installing applications using AppGet - Gaming"
     $apps = @(
         "battlenet"
-        #"geforce-experience" # nvidia only
         "gog-galaxy"
         "origin"
         "steam"
@@ -104,20 +111,44 @@ if (-not ($computerDetails.IsVirtual)) {
 
 ## Other AppGet packages available, installed only as needed
 ##
+# audacity -> sound editing app
 # blender -> 3d graphics editor
 # blockbench -> 3d model editor
 # clementine -> music organizer\player
+# cryptomator -> encryption tool to be used with file sync tools
 # gimp -> photo editing
+# gnucash -> financial app
 # harmony -> electron based music player, cross-plat
 # inkscape -> vector graphics editor
+# jitsi -> open source video conferencing
 # kdenlive -> movie\video editor
 # krita -> natural paint program
 # libreoffice -> office suite
+# mattermost -> open source version of slack
+# meld -> open source replacement for beyond compare
 # mp3tag -> what it sounds like
+# mumble -> open source voice chat
 # musescore -> music notation
 # nextcloud -> the clinet app, if I ever get a server running
 # obs-studio -> screen cam\screen recording
+# openvpn -> open source vpn software
+# patchwork -> open source secure chat app
 # picard -> cross-plat music tagging tool
+# pidgin -> chat app
+# qtox -> another secure messaging app
+# scribus -> open source desktop publishing app
 # shotcut -> image\photo editor
+# skype
 # slack
+# strawberry-music-player
+# tagspaces
+# telegram
+# thomas -> open source cross-plat pomodoro timer
 # twitch -> desktop client for the service
+# udeler -> udemy course downloader
+#
+# GNU tools
+# grep
+# gzip
+# sed
+# wget
