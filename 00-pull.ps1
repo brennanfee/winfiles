eca7220fbe4680ef40a42790b5e3a51593b664a9
Brennan Fee <brennan@github.mailbots.org>
2019-09-16 20:49:28 -0500

    Fixing path to shared script in pull script

--- 00-pull.ps1
+++ 00-pull.ps1
@@ -87,7 +87,7 @@ else {
     Write-Host "Winfiles already set up." -ForegroundColor "Green"
 }
 
-Invoke-Expression -command "$env:PROFILEPATH\winfiles\set-machine-type.ps1"
+Invoke-Expression -command "$env:PROFILEPATH\winfiles\shared\set-system-type.ps1"
 
 Set-Location "$env:PROFILEPATH\winfiles"
 Write-Host "WinFiles are ready, you may now run .\01-setup-profile.ps1" -ForegroundColor "Yellow"
