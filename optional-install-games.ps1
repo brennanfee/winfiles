79e8d61173e4422b5715e19c32a05b0587c5f050
Brennan Fee <brennan@github.mailbots.org>
2020-03-28 02:19:27 -0500

    refactor(choco): Move game script to generic folder
    Renaming the gameInstallScripts to a more generic optionalInstallScripts
    in anticipation of wanting to add a development package as well.  This
    is a better design anyway as it allows any number of optional packets of
    application installs.

--- optional-install-games.ps1
+++ optional-install-games.ps1
@@ -14,7 +14,7 @@ Write-LogAndConsole $logFile "System type: $env:SYSTEMTYPE"
 Write-LogAndConsole $logFile ""
 
 Write-Host "Installing applications using Chocolatey - Gaming"
-Invoke-Expression "choco install -y -r `"$PSScriptRoot\gameInstallScripts\choco-gaming-apps.xml`""
+Invoke-Expression "choco install -y -r `"$PSScriptRoot\optionalInstallScripts\choco-gaming-apps.xml`""
 
 Write-Host ""
 Write-LogAndConsole $logFile "A reboot will be necessary (again)." -Color "Yellow"
