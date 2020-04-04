dcd386e704359946df459eeef43a5777f343f8c7
Brennan Fee <brennan@github.mailbots.org>
2020-04-03 19:14:14 -0500

    fix: Reverting some path modifications
    We can't always return paths wrapped in quotes as some things (like
    adding to path) can't handle the extra quotes.  So, the burden of
    handling paths with spaces has to come on the invoking of the command.

--- powershell-modules/SystemUtilities/EditorUtilities.ps1
+++ powershell-modules/SystemUtilities/EditorUtilities.ps1
@@ -83,7 +83,7 @@ function Set-EditorToAtom {
 }
 function Set-EditorToNotepad {
     Write-Host -ForegroundColor 'Green' "Setting editor to notepad."
-    Set-Editor("""C:\Windows\system32\notepad.exe""")
+    Set-Editor("C:\Windows\system32\notepad.exe")
     return $true
 }
 
@@ -99,8 +99,7 @@ function Get-ApplicationPath {
     $command = Get-Command -CommandType Application -ErrorAction Ignore -Name "$Application"
     if ($command) {
         if (Test-Path $command.Source) {
-            $appPath = $command.Source
-            return """$appPath"""
+            return $command.Source
         }
     }
 
@@ -113,10 +112,10 @@ function Get-DefaultSublimeTextExe {
         return $app
     }
     elseif (Test-Path "C:\Program Files\Sublime Text 3\sublime_text.exe") {
-        return """C:\Program Files\Sublime Text 3\sublime_text.exe"""
+        return "C:\Program Files\Sublime Text 3\sublime_text.exe"
     }
     elseif (Test-Path "C:\Program Files (x86)\Sublime Text 3\sublime_text.exe") {
-        return """C:\Program Files (x86)\Sublime Text 3\sublime_text.exe"""
+        return "C:\Program Files (x86)\Sublime Text 3\sublime_text.exe"
     }
     else {
         return ""
@@ -129,7 +128,7 @@ function Get-DefaultAtomExe {
         return $app
     }
     elseif (Test-Path "$env:LOCALAPPDATA\atom\atom.exe") {
-        return """$env:LOCALAPPDATA\atom\atom.exe"""
+        return "$env:LOCALAPPDATA\atom\atom.exe"
     }
     else {
         return ""
@@ -142,13 +141,13 @@ function Get-DefaultVSCodeExe {
         return $app
     }
     elseif (Test-Path "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code") {
-        return """$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code"""
+        return "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code"
     }
     elseif (Test-Path "C:\Program Files\Microsoft VS Code\bin\code") {
-        return """C:\Program Files\Microsoft VS Code\bin\code"""
+        return "C:\Program Files\Microsoft VS Code\bin\code"
     }
     elseif (Test-Path "C:\Program Files (x86)\Microsoft VS Code\bin\code") {
-        return """C:\Program Files (x86)\Microsoft VS Code\bin\code"""
+        return "C:\Program Files (x86)\Microsoft VS Code\bin\code"
     }
     else {
         return ""
@@ -169,10 +168,10 @@ function Get-DefaultVimExe {
             "73"
         ) | ForEach-Object {
             if (Test-Path "C:\Program Files\Vim\vim$_\gvim.exe") {
-                return """C:\Program Files\Vim\vim$_\gvim.exe"""
+                return "C:\Program Files\Vim\vim$_\gvim.exe"
             }
             elseif (Test-Path "C:\Program Files (x86)\Vim\vim$_\gvim.exe") {
-                return """C:\Program Files (x86)\Vim\vim$_\gvim.exe"""
+                return "C:\Program Files (x86)\Vim\vim$_\gvim.exe"
             }
         }
     }
@@ -186,10 +185,10 @@ function Get-DefaultEmacsExe {
         return $app
     }
     elseif (Test-Path "C:\Program Files\Emacs\bin\emacsclientw.exe") {
-        return """C:\Program Files\Emacs\bin\emacsclientw.exe"""
+        return "C:\Program Files\Emacs\bin\emacsclientw.exe"
     }
     elseif (Test-Path "C:\Program Files (x86)\Emacs\bin\emacsclientw.exe") {
-        return """C:\Program Files (x86)\Emacs\bin\emacsclientw.exe"""
+        return "C:\Program Files (x86)\Emacs\bin\emacsclientw.exe"
     }
     else {
         return ""
